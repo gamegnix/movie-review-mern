d3adb7ae320906c73a1267adf6607fe1e2af84cd
gamegnix <ameenshajahan1224@gmail.com>
2026-01-06 22:07:10 +0530

    Backend connected to MongoDB Atlas and APIs tested

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -7,7 +8,8 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/movieReviewDB")
+mongoose.connect(process.env.MONGO_URI)
+
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
